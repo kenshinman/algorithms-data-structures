ea6cc5c28d92b60e59bd70a614ac38488405a628
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-01 17:02:37 -0400

    01-04 is done

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,9 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  // Check if the operator is valid
+  if (!["+", "-", "*", "/"].includes(operator)) {
+    throw new Error("Invalid operator. Please use +, -, *, or /.");
+  }
+  return Number(eval(`${num1} ${operator} ${num2}`));
+}
 
 module.exports = calculator;
--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,12 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let count = 0;
+  str.split("").forEach((c) => {
+    console.log(c, char);
+    if (c === char) {
+      count++;
+    }
+  });
+  return count;
+}
 
 module.exports = countOccurrences;
