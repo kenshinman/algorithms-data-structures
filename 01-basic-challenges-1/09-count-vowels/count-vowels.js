6de0edf4fab2ca4cdb36fd63089832331221c901
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-02 18:32:05 -0400

    finished 01-09

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,12 @@
-function countVowels() {}
+function countVowels(str) {
+  const vowels = "aeiou";
+  let count = 0;
+  for (let i = 0; i <= str.length - 1; i++) {
+    if (vowels.includes(str[i].toLowerCase())) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
