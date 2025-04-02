0ef5662791d296d2bd434a5f6ecd6bb64091ecee
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-02 19:57:32 -0400

    done with 01-10

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,18 @@
-function removeDuplicates() {}
+function removeDuplicates(items) {
+  // most efficient way to remove duplicates from an array in JavaScript
+  // using Set constructor which creates a new Set object
+  //return [...new Set(items)];
+
+  // using filter method to remove duplicates from an array in JavaScript
+  //return items.filter((item, index) => items.indexOf(item) === index);
+  // using loops
+  let result = [];
+  for (let i = 0; i < items.length; i++) {
+    if (result.indexOf(items[i]) === -1) {
+      result.push(items[i]);
+    }
+  }
+  return result;
+}
 
 module.exports = removeDuplicates;
