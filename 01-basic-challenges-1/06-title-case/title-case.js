35910a2ed9c1cb47314819da655d63189ee268ec
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-02 17:41:09 -0400

    01-06 done

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,10 @@
-function titleCase() {}
+function titleCase(str) {
+  return str
+    .split(" ")
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join(" ");
+}
 
 module.exports = titleCase;
