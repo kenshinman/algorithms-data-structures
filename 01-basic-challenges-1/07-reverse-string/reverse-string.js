a9da93a3fbcb198aff564e88a44ec9d954cdcb54
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-02 17:51:14 -0400

    done with 01-07

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,14 @@
-function reverseString() {}
+function reverseString(str) {
+  // most efficient solution
+  // return str.split("").reverse().join("");
+
+  //using for loop
+  let reversed = "";
+  const splitStr = str.split("");
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += splitStr[i];
+  }
+  return reversed;
+}
 
 module.exports = reverseString;
