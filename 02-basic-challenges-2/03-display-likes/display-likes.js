cdcd17d4e9349f7ac03432fbb8af43fd0b4f5e77
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-02 20:56:39 -0400

    02-03 done

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,14 @@
-function displayLikes() {}
+function displayLikes(namesArr) {
+  const namesArrLength = namesArr.length;
+  if (namesArrLength === 0) return `no one likes this`;
+  if (namesArrLength === 1) return `${namesArr[0]} likes this`;
+  if (namesArrLength === 2)
+    return `${namesArr[0]} and ${namesArr[1]} like this`;
+  if (namesArrLength === 3)
+    return `${namesArr[0]}, ${namesArr[1]} and ${namesArr[2]} like this`;
+  return `${namesArr[0]}, ${namesArr[1]} and ${
+    namesArrLength - 2
+  } others like this`;
+}
 
 module.exports = displayLikes;
