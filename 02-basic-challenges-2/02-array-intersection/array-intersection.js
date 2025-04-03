bd6968c009b9453a59d5414dc15861afc8adc789
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-02 20:44:56 -0400

    02-02 done

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,11 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const intersection = [];
+  arr1.forEach((item, i) => {
+    if (arr2.includes(item) && !intersection.includes(item)) {
+      intersection.push(item);
+    }
+  });
+  return intersection;
+}
 
 module.exports = arrayIntersection;
