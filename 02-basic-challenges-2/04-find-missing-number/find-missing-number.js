bd5ac7eb44d2bd600f394fbbdc54454fba05f19f
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-07 12:31:03 -0400

    updated

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,11 @@
-function findMissingNumber() {}
+function findMissingNumber(numArr) {
+  let missingNum;
+  for (let i = 1; i <= numArr; i++) {
+    if (!numArr.includes(i)) {
+      missingNum = i;
+    }
+  }
+  return missingNum;
+}
 
 module.exports = findMissingNumber;
