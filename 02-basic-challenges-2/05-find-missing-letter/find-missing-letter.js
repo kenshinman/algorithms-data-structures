5d73f412460027e97cfa350fa7899b54c0ce1dc1
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-07 17:46:48 -0400

    done with 02-05

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,12 @@
-function findMissingLetter() {}
+function findMissingLetter(strArr) {
+  const alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const firstIndex = alphabets.indexOf(strArr[0]);
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] !== alphabets[firstIndex + i]) {
+      return alphabets[firstIndex + i];
+    }
+  }
+  return "";
+}
 
 module.exports = findMissingLetter;
