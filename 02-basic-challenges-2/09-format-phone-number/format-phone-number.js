2d5b4bd27ead61cb1354a7c30d6c09ec24bdccce
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-07 19:34:38 -0400

    done with 02 - 09

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,8 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numArr) {
+  const firstChunk = numArr.slice(0, 3).join(""),
+    secondChunk = numArr.slice(3, 6).join(""),
+    thirdChunk = numArr.slice(-4).join("");
+  return `(${firstChunk}) ${secondChunk}-${thirdChunk}`;
+}
 
 module.exports = formatPhoneNumber;
