95ec7ff76ef11ed1add43b800d53bdaef308a0e1
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-07 17:06:32 -0400

    updated missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,12 +1,27 @@
 function findMissingNumber(numArr) {
-  const largestNumber = numArr.sort((a, b) => a - b)[numArr.length - 1];
+  if (!numArr.length) return 1;
   let missingNum;
-  for (let i = 1; i <= largestNumber; i++) {
-    if (numArr.indexOf(i) === -1) {
-      missingNum = i;
-    }
-  }
-  return missingNum;
+  const largestNumber = numArr.sort((a, b) => a - b)[numArr.length - 1];
+
+  // method one
+  // for (let i = 1; i <= largestNumber; i++) {
+  //   if (numArr.indexOf(i) === -1) {
+  //     missingNum = i;
+  //   }
+  // }
+
+  // method two
+  // the difference between the sum of both arrays is the missing number
+  const n = numArr.length + 1;
+
+  const expectedSum = (n * (n + 1)) / 2;
+  const sumOfNumArr = numArr.reduce((a, b) => {
+    return a + b;
+  }, 0);
+
+  return expectedSum - sumOfNumArr;
+
+  // return missingNum;
 }
 
 module.exports = findMissingNumber;
