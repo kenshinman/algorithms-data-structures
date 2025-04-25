ef7088bef93cb0c43f5eaa33c12d623202900e3e
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-25 17:00:04 -0400

    03-03

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,8 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numArr) {
+  return numArr
+    .filter((num) => num > 0 && num % 2 === 0)
+    .map((num) => Math.pow(num, 2))
+    .reduce((init, num) => init + num, 0);
+}
 
 module.exports = sumOfEvenSquares;
--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,10 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  return products.reduce((acc, item) => {
+    const totalLinePrice = item.price * item.quantity;
+    const itemTax = totalLinePrice * (taxRate / 100);
+    const totalLineWithTax = totalLinePrice + itemTax;
+    return parseFloat(acc + totalLineWithTax);
+  }, 0);
+}
 
 module.exports = calculateTotalSalesWithTax;
