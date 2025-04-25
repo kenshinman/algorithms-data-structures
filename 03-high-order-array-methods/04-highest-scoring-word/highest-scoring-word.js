50e1ea4477b0d11e4cec4c9b03969c44be04cefa
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-25 17:53:03 -0400

    03-04+05

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,18 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  const letters = "_abcdefghijklmnopqrstuvwxyz";
+
+  const splitWords = str.toLowerCase().split(" ");
+
+  const numArr = splitWords.map((word) => {
+    return (wordTotal = word.split("").reduce((acc, letter) => {
+      return acc + letters.indexOf(letter);
+    }, 0));
+  });
+  const highestValue = numArr.sort((a, b) => (a > b ? 1 : -1))[
+    numArr.length - 1
+  ];
+
+  return splitWords[numArr.indexOf(highestValue)];
+}
 
 module.exports = highestScoringWord;
--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,7 @@
-function validAnagrams() {}
+function validAnagrams(word1, word2) {
+  const sortedWord1 = word1.split("").sort().join("");
+  const sortedWord2 = word2.split("").sort().join("");
+  return sortedWord1 === sortedWord2;
+}
 
 module.exports = validAnagrams;
