8a40bf6bb7f7e707cf32b5cec0d2eb3fb8ff4743
Kehinde Orilogbon <archkenshin@gmail.com>
2025-04-29 19:05:46 -0400

    03-08 done

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,10 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (ipString) => {
+  const splitStr = ipString.split(".");
+  if (splitStr.length !== 4) return false;
+  return splitStr.every((str) => {
+    const num = parseInt(str);
+    return num > -1 && num < 256 && !str.startsWith("0");
+  });
+};
 
 module.exports = isValidIPv4;
--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,26 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((acc, car) => {
+    return (acc += car.mileage);
+  }, 0);
+  const averageMileage = totalMileage / cars.length;
+  const highestMileage = Math.max(...cars.map((car) => car.mileage));
+  const lowestMileage = Math.min(...cars.map((car) => car.mileage));
+  const highestMileageCar = cars.find((car) => car.mileage === highestMileage);
+  const lowestMileageCar = cars.find((car) => car.mileage === lowestMileage);
+
+  console.log({
+    totalMileage,
+    highestMileageCar,
+    lowestMileageCar,
+    averageMileage,
+  });
+
+  return {
+    totalMileage,
+    highestMileageCar,
+    lowestMileageCar,
+    averageMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
